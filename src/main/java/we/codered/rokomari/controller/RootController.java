4dc848cf3561b00ef43bd54e34c556650ad98b88
mastersunny <sunnycse248@gmail.com>
2015-11-24 13:11:08 +0600

    homepage added

--- src/main/java/we/codered/rokomari/controller/RootController.java
+++ src/main/java/we/codered/rokomari/controller/RootController.java
@@ -1,5 +1,16 @@
 package we.codered.rokomari.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class RootController {
+	
+	@RequestMapping("/")
+	public String home(){
+		
+		return "home";
+		
+	}
 
 }
