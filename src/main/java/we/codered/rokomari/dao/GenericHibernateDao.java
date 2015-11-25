bcdb12d8722211315fce34d1ad860f339dc175b5
Shafin Mahmud <shafin.mahmud@gmail.com>
2015-11-26 02:09:42 +0600

    file uploader added

--- src/main/java/we/codered/rokomari/dao/GenericHibernateDao.java
+++ src/main/java/we/codered/rokomari/dao/GenericHibernateDao.java
@@ -5,7 +5,6 @@ import java.io.Serializable;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
 
-@Repository
 @Scope("protoType")
 public class GenericHibernateDao <T extends Serializable> extends AbstractHibernateDao<T> {
 	
---
+++ src/main/java/we/codered/rokomari/util/MultiFileValidator.java
@@ -0,0 +1,33 @@
+package we.codered.rokomari.util;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import we.codered.rokomari.bean.FileBucket;
+import we.codered.rokomari.bean.MultiFileBucket;
+
+
+@Component
+public class MultiFileValidator implements Validator {
+    
+   public boolean supports(Class<?> clazz) {
+       return MultiFileBucket.class.isAssignableFrom(clazz);
+   }
+
+   public void validate(Object obj, Errors errors) {
+       MultiFileBucket multiBucket = (MultiFileBucket) obj;
+        
+       int index=0;
+        
+       for(FileBucket file : multiBucket.getFiles()){
+           if(file.getFile()!=null){
+               if (file.getFile().getSize() == 0) {
+                   errors.rejectValue("files["+index+"].file", "missing.file");
+               }
+           }
+           index++;
+       }
+        
+   }
+}
